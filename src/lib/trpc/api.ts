9b765ea6476b2985514322940955584270cb254e
Abinash Panda <abinash@prodios.com>
2023-09-18 19:10:12 +0530

    feat(trpc): revert back to nextHttpLink

--- src/lib/trpc/api.ts
+++ src/lib/trpc/api.ts
@@ -1,9 +1,10 @@
 import { experimental_createTRPCNextAppDirServer as createTRPCNextAppDirServer } from '@trpc/next/app-dir/server'
-import { experimental_nextCacheLink as nextCacheLink } from '@trpc/next/app-dir/links/nextCache'
+import { experimental_nextHttpLink as nextHttpLink } from '@trpc/next/app-dir/links/nextHttp'
 import SuperJSON from 'superjson'
 import { loggerLink } from '@trpc/client'
-import { AppRouter, appRouter } from '@/server/api/router'
-import { createTRPCContext } from '@/server/api/context'
+import { cookies } from 'next/headers'
+import { AppRouter } from '@/server/api/router'
+import { getUrl } from '../utils'
 
 export const api = createTRPCNextAppDirServer<AppRouter>({
   config() {
@@ -12,10 +13,15 @@ export const api = createTRPCNextAppDirServer<AppRouter>({
         loggerLink({
           enabled: (opts) => opts.direction === 'down' && opts.result instanceof Error,
         }),
-        nextCacheLink({
-          revalidate: 1,
-          router: appRouter,
-          createContext: createTRPCContext,
+        nextHttpLink({
+          url: getUrl('/api/trpc'),
+          batch: true,
+          headers() {
+            return {
+              cookie: cookies().toString(),
+              'x-trpc-source': 'rsc-invoke',
+            }
+          },
         }),
       ],
       transformer: SuperJSON,
